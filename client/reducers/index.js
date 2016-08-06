a36232abc2ba7961793a7214a82ee6a481f46450
Giorgio Aquino <gigetto@gmail.com>
2016-08-06 15:21:40 +0200

    Update 2016-08-06T13:21:38.654Z

--- client/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import password from './password';
-
-// Reducers list (just one for now)
-const spicyPasswordApp = combineReducers({
-  password,
-});
-
-export default spicyPasswordApp;
