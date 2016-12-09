f3270e7c288464fc038c8fe88d2a902b2ac78898
Giorgio Aquino <gigetto@gmail.com>
2016-12-09 23:36:04 +0100

    removed color store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import password from './password';
 import engines from './engines';
-import colors from './colors';
 
 // Reducers list (just one for now)
 const spicyPasswordApp = combineReducers({
   password,
   engines,
-  // colors,
 });
 
 export default spicyPasswordApp;
