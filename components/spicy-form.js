11f7bf6103cc263d8e2d605b9e4a06f65163d056
G100g <giorgio@g100g.net>
2021-04-11 21:45:37 +0200

    feat: save services on localstorage

--- components/spicy-form.js
+++ components/spicy-form.js
@@ -3,6 +3,29 @@ import { LayoutContext } from "../context/layout";
 import passwordEngines from "../core/spicy-rack";
 import { useCopyToClipboard } from "../hooks/useCopyToClipboard";
 
+const saveService = (service) => {
+  const spicyServices = getSavedServices();
+
+  spicyServices.push(service);
+
+  window.localStorage.setItem(
+    "spicy-services",
+    JSON.stringify([...new Set(spicyServices)])
+  );
+};
+
+const getSavedServices = () => {
+  const services =
+    (global.window && (window.localStorage.getItem("spicy-services") ?? [])) ||
+    [];
+
+  try {
+    return JSON.parse(services);
+  } catch (e) {
+    return [];
+  }
+};
+
 const useSpicyPassword = () => {
   const [masterpassword, setMasterpassword] = useState("");
   const [service, setService] = useState("");
@@ -22,7 +45,12 @@ function SpicyForm() {
   const [copied, copy] = useCopyToClipboard(spicyPassword);
   const layout = useContext(LayoutContext);
 
-  layout.setColorByDomain(`maincontent--${service.replace(/\./gi, "-")}`);
+  if (service != "") {
+    layout.setColorByDomain(`maincontent--${service.replace(/\./gi, "-")}`);
+  }
+
+  const spicyServices = getSavedServices();
+
   return (
     <form>
       <fieldset className="maincontent__password">
@@ -43,17 +71,29 @@ function SpicyForm() {
           placeholder="facebook.com"
           // defaultValue={password.domain}
           onChange={({ target }) => setService(target.value)}
-          autocorrect="off"
-          autocapitalize="none"
-          autocomplete="off"
+          autoCorrect="off"
+          autoCapitalize="none"
+          autoComplete="off"
+          list="spicy-services"
         />
+        <datalist id="spicy-services">
+          {spicyServices.map((spicyService) => (
+            <option>{spicyService}</option>
+          ))}
+        </datalist>
       </fieldset>
       <fieldset>
         <label htmlFor="result">Your spicy password</label>
         <input id="result" type="text" value={spicyPassword} readOnly />
       </fieldset>
       <fieldset>
-        <button type="button" onClick={copy}>
+        <button
+          type="button"
+          onClick={() => {
+            copy();
+            saveService(service);
+          }}
+        >
           {copied ? `✅ Copied` : `Copy`} to clipboard
         </button>
       </fieldset>
